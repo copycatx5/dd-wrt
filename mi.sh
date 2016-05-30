801e8ab7a91e954046858d443d4ca39c541925bd
fkpwolf <fkpwolf@gmail.com>
2016-05-30 13:13:37 +0800

    modified:   mi.sh

--- mi.sh
+++ mi.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-#GCC_DIR=~/OpenWrt-Toolchain-bcm53xx-for-arm_cortex-a9-gcc-4.8-linaro_uClibc-0.9.33.2_eabi/toolchain-arm_cortex-a9_gcc-4.8-linaro_uClibc-0.9.33.2_eabi
-#musl toolchain
-#GCC_DIR=~/openwrt/staging_dir/toolchain-arm_cortex-a9_gcc-4.9.0_musl-1.1.4_eabi
-
 #DD-WRT's toolchains
-GCC_DIR=~/toolchains-dd-wrt/toolchain-arm_cortex-a9_gcc-4.9-linaro_musl-1.1.2_eabi
-#GCC_DIR=~/openwrt/staging_dir/toolchain-arm_cortex-a9_gcc-4.9.0_musl-1.1.4_eabi
+GCC_DIR=/mnt/blue1t/OpenWrt-Toolchain-bcm53xx_gcc-5.3.0_musl-1.1.14_eabi.Linux-x86_64/toolchain-arm_cortex-a9_gcc-5.3.0_musl-1.1.14_eabi
 
 [ -e $GCC_DIR/bin/arm-linux-uclibc-gcc ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-gcc $GCC_DIR/bin/arm-linux-uclibc-gcc
 [ -e $GCC_DIR/bin/arm-linux-uclibc-ar ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-ar $GCC_DIR/bin/arm-linux-uclibc-ar
@@ -18,6 +13,18 @@ GCC_DIR=~/toolchains-dd-wrt/toolchain-arm_cortex-a9_gcc-4.9-linaro_musl-1.1.2_ea
 [ -e $GCC_DIR/bin/arm-linux-uclibc-strip ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-strip $GCC_DIR/bin/arm-linux-uclibc-strip
 [ -e $GCC_DIR/bin/arm-linux-uclibc-size ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-size $GCC_DIR/bin/arm-linux-uclibc-size
 [ -e $GCC_DIR/bin/arm-linux-uclibc-objcopy ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-objcopy $GCC_DIR/bin/arm-linux-uclibc-objcopy
+
+
+[ -e $GCC_DIR/bin/arm-linux-gcc ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-gcc $GCC_DIR/bin/arm-linux-gcc
+[ -e $GCC_DIR/bin/arm-linux-ar ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-ar $GCC_DIR/bin/arm-linux-ar
+[ -e $GCC_DIR/bin/arm-linux-ranlib ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-ranlib $GCC_DIR/bin/arm-linux-ranlib
+[ -e $GCC_DIR/bin/arm-linux-ld ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-ld $GCC_DIR/bin/arm-linux-ld
+[ -e $GCC_DIR/bin/arm-linux-nm ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-nm $GCC_DIR/bin/arm-linux-nm
+[ -e $GCC_DIR/bin/arm-linux-g++ ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-g++ $GCC_DIR/bin/arm-linux-g++
+[ -e $GCC_DIR/bin/arm-linux-strip ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-strip $GCC_DIR/bin/arm-linux-strip
+[ -e $GCC_DIR/bin/arm-linux-size ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-size $GCC_DIR/bin/arm-linux-size
+[ -e $GCC_DIR/bin/arm-linux-objcopy ] || ln -sf $GCC_DIR/bin/arm-openwrt-linux-objcopy $GCC_DIR/bin/arm-linux-objcopy
+
 
 export PATH=$GCC_DIR/bin:$PATH
 
